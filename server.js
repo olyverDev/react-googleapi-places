052434e86b5ce24a406fc1592915564fb8f422e9
Oleg Veremejchik <oliver10010111@gmail.com>
2017-03-30 21:35:15 +0300

    made base post methods for app which make requests to google api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const http = require('http');
 const config = require('./webpack.config');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const request = require('request')
+const bp = require('body-parser')
 const app = express();
 const compiler = webpack(config);
 
@@ -21,6 +23,45 @@ http.createServer(app)
     console.log('Local server listening to port ' + app.get('port'));
   });
 
+app.use(bp.json());
+app.use(bp.urlencoded({
+  extended: true
+}));
+
 app.get('*', function (req, res) {
   res.status(404).send('error 404, page not found');
+});
+
+app.post('/locations', function (req, res) {
+  var foreignUrl = req.body.mainUrl + 'location=' + req.body.location +
+    '&radius=' + req.body.radius + '&types=' + req.body.types + '&key=' + req.body.key;
+  console.log('/locations url: ' + foreignUrl)
+
+  request(foreignUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) 
+      res.send(body);
+    else 
+      console.log('Some error! Check internet connection');
+  });
+});
+
+app.post('/extraLocations', function (req, res) {
+  var foreignUrl = req.body.mainUrl + 'pagetoken=' + req.body.pagetoken + '&key=' + req.body.key;
+  console.log('/extraLocations url: ' + foreignUrl)
+  request(foreignUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) 
+      res.send(body);
+  })
+});
+
+app.post('/locationAutocomplete', function (req, res) {
+  var foreignUrl = req.body.mainUrl +'input='+ req.body.input + '&language=' 
+  + req.body.language + '&key=' + req.body.key;
+  console.log('hintsUrl: ' + foreignUrl)
+  request(foreignUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200)
+      res.send(body);
+      else 
+      console.log('Some error! Check internet connection');
+  })
 });
