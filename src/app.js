c56d85301e09be2676f3ef5093ced76fa4032b3f
Oleg Veremejchik <oliver10010111@gmail.com>
2017-04-13 21:49:24 +0300

    changed App component due to restructuring of other components.Deleted useless imports

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 import Filter from './components/filter'
-import LocationList from './components/locationList'
 import FilterResult from './components/filterResult'
-import LocationAutocomplete from './components/locationAutocomplete'
+import MapAndLocations from './components/mapAndLocations'
+// import Map from './components/map'
 
 export default class App extends Component {
     render () {
         return (
             <div>
-            <Filter />
-            <LocationList />
+            <Filter /> 
             <FilterResult />
-            <LocationAutocomplete />
+            <MapAndLocations />
             </div>
         );
     }
