bcdead4d7627438d5379da08969ed9e9800fe19a
Oleg Veremejchik <oliver10010111@gmail.com>
2017-04-19 13:51:22 +0300

    tested react-router-dom; left this code in comment for future modifications

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+/*import Filter from './components/filter'
+import FilterResult from './components/filterResult'
+import MapAndLocations from './components/mapAndLocations'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 render (
-    <App />,
+    <Router>
+      <div>
+      <Route exact path="/" component={App}/>
+      <Route path="/locations" component={App}/>
+      <Route path="/autocomplete" component={App}/>
+      </div>
+  </Router>,
+    document.getElementById('root')
+)*/
+
+render (
+  <App />,
     document.getElementById('root')
 )
 
