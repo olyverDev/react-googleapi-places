9c675711456d0791214b3fdd69713ebe427397fc
Oleg Veremejchik <oliver10010111@gmail.com>
2017-04-19 13:48:13 +0300

    added const for response language; removed google key const, because it needs to be hidden from user

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
-const GOOGLE_KEY = 'AIzaSyBXzTLkiIpYXc_I6D-XKPM7npbKQk0uhfE';
 const headers = {
   "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
 };
 const DEFAULT_LAT_LNG = '53.669354,23.813131';
 const SEARCH_FIELD = document.getElementById('searchField');
+const responseLanguage = 'en';
 
 
-export { GOOGLE_KEY, headers, DEFAULT_LAT_LNG, SEARCH_FIELD }
+export { headers, DEFAULT_LAT_LNG, SEARCH_FIELD, responseLanguage }
